76f26ff4c491850c8579c492831a774efc7d8231
Zhang Lingyun <zhang.lingyun@pwc.com>
2019-12-11 21:58:29 +0800

    init commit

---
+++ src/main/java/com/example/ziragraphql/ZiraGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.ziragraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiraGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiraGraphqlApplication.class, args);
+	}
+
+}
